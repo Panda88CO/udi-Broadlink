a7c6f588e7e0f9f2723de88ab50406cc4206f627
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2026-04-23 18:18:09 -0700

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -745,7 +745,7 @@ class BroadlinkController(BaseNode):
             try:
                 LOGGER.debug("[_publish_profile] Attempting to retrieve current profile from IoX")
                 current_profile = current_profile_getter({"waitResponse": True})
-                LOGGER.debug("[_publish_profile] Current JSON profile from IoX: %s", json.dumps(current_profile, sort_keys=True))
+                LOGGER.debug("[_publish_profile] Current JSON profile from IoX: %s", json.dumps(current_profile, sort_keys=True), indent=2, separators=(",", ": "))
                 if self._profiles_match(current_profile, profile):
                     LOGGER.info("[_publish_profile] JSON profile already up to date, skipping publish")
                     return
