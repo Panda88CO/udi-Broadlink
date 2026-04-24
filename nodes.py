b055622457b6415a00db03b7ef398befe4c87016
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2026-04-23 18:16:01 -0700

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -753,7 +753,7 @@ class BroadlinkController(BaseNode):
             except TypeError:
                 LOGGER.debug("[_publish_profile] getJsonProfile does not support waitResponse, trying without")
                 current_profile = current_profile_getter()
-                LOGGER.debug("[_publish_profile] Current JSON profile from IoX: %s", json.dumps(current_profile, sort_keys=True))
+                LOGGER.debug("[_publish_profile] Current JSON profile from IoX: %s", json.dumps(current_profile, sort_keys=True), indent=2, separators=(",", ": "))
                 if self._profiles_match(current_profile, profile):
                     LOGGER.info("[_publish_profile] JSON profile already up to date, skipping publish")
                     return
